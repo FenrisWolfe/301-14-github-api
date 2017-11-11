cbbde69cbbf0b1eb964aaf39f546f8f2c02f9e70
FREDRIC THOMAS RAPP <fred.rapp@gmail.com>
2017-11-11 07:15:59 -0800

    all working with token exported and ready to deploy

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
+/*jshint esversion: 6 */
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 const cors = require('cors');
+const superagent = require('superagent');
 
 app.use(express.static('./public'));
 app.use(cors());
 
+app.get('/github/*', (req, res) => {
+  console.log('routing a github API request for', req.params[0]);
+  const url = `https://api.github.com/${req.params[0]}`;
+  superagent(url)
+  .set(`Authorization`, `token ${process.env.GITHUB_TOKEN}`)
+  .then(
+    repos => res.send(repos.text),
+    err => res.send(err)
+  );
+});
+
 app.get('/', (req, res) => {
-  res.sendFile('index.html')
+  res.sendFile('index.html');
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
