3d82a200b487cceb96bda108a06fddb4ecf2f699
FREDRIC THOMAS RAPP <fred.rapp@gmail.com>
2017-11-12 18:20:36 -0800

    added my stuff

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 // Let's make an AJAX call to the GitHub API and then do a simple render of the data into the DOM
 
-$.get('/github/user/repos')
+$.get('/github/user/FenrusWolfe/repos')
   .then(
     data => JSON.parse(data).forEach(repo =>
       $('#results').append(`<h3>${repo.name}</h3><p>${repo.description}</p><hr>`)),
